db30d8c69351e8b5a3aa5da6b9e0a5625e422f48
Martin VanWinkle III <mvanwinkle@ias.edu>
2019-01-06 19:08:55 -0500

    adding a test package.

---
+++ tests/ias_package_shell_test_package_a/doc/index.md
@@ -0,0 +1,3 @@
+# ias_package_shell_test_package_a
+
+This is the supplemental root documentation for ias_package_shell_test_package_a
--- tests/test_a.sh
+++ tests/test_a.sh
@@ -3,7 +3,7 @@
 
 DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cat $DIRNAME/answers.txt | $DIRNAME/../src/bin/package_shell.pl
+cat $DIRNAME/answers.txt | $DIRNAME/../src/bin/ias_package_shell.pl
 chmod +x ias_test_package/src/bin/hello.sh
 
 echo ""
