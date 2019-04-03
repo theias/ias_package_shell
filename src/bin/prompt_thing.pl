5b22fd3b559036ab076d41b35a8ce934017cff58
Martin VanWinkle <mvanwinkle@ias.edu>
2019-04-03 11:31:01 -0400

    progress

--- src/bin/prompt_thing.pl
+++ src/bin/prompt_thing.pl
@@ -9,6 +9,7 @@ use IO::File;
 use Getopt::Long;
 use Data::Dumper;
 
+our $SUPPRESS_DEFAULT_NOTIFICATIONS=1;
 my $file_name = $ARGV[0]
 	or die "First arg is json file.\n";
 
@@ -25,19 +26,51 @@ sub do_prompts
 	my $data = {};
 	THIS_PROMPT: foreach my $prompt (@$prompts)
 	{
-		if ($prompt->{'description'})
+		my $response;
+		if (! $prompt->{'dont_prompt'})
 		{
-			print $prompt->{"description"},$/
+			if ($prompt->{'description'})
+			{
+				print $prompt->{"description"},$/
+			}
+		
+			print $prompt->{"prompt"};
+		
+			if ( defined ($prompt->{"default_value"}) )
+			{
+				print '[',$prompt->{"default_value"},'] ';
+			}
+		
+			$response = <STDIN>;
+			chomp($response);
+		}
+
+		my $used_default = 0;
+		if (
+			defined ($prompt->{"default_value"})
+			&& (! defined $response || $response =~ m/^\s*/ ) 
+		)
+		{
+			if (! $SUPPRESS_DEFAULT_NOTIFICATIONS)
+			{
+				print "Using default value for ",$prompt->{'prompt'}, " [",
+					$prompt->{'default_value'}, ']',$/;
+			}
+			$response = $prompt->{"default_value"};
+			
+			$used_default = 1;
 		}
 		
-		print $prompt->{"prompt"};
-		my $response = <STDIN>;
-		chomp($response);
 		my $fail_regex = $prompt->{'fail_regex'};
 		
 		if ( $fail_regex && $response =~ m/$fail_regex/)
 		{
 			print "$response matches fail regex: $fail_regex.  Please try again.\n";
+			if ($used_default)
+			{
+				print "Additionally, the default value appears to be bad.\n";
+				$prompt->{'dont_prompt'} = 0;
+			}
 			redo THIS_PROMPT;
 		}
 		
@@ -45,6 +78,11 @@ sub do_prompts
 		if ($pass_regex && $response !~ m/$pass_regex/)
 		{
 			print "$response does not match pass regex: $pass_regex.  Please try again.\n";
+			if ($used_default)
+			{
+				print "Additionally, the default value appears to be bad.\n";
+				$prompt->{'dont_prompt'} = 0;
+			}
 			redo THIS_PROMPT;
 		}
 		
