bf0d6aa8dd9a5ff62a03e39405245bb21a1f7466
Martin VanWinkle III <mvanwinkle@ias.edu>
2023-02-01 13:03:08 -0500

    progress

--- src/templates/project_dir/tests/bin/a_test.sh
+++ src/templates/project_dir/tests/bin/a_test.sh
@@ -7,8 +7,10 @@ my_lib_dir=${my_lib_dir:-"$this_dir/../lib"}
 . "$test_etc_dir/test_config.bash"
 # End bootstrap
 
+# This dumps some of the variables that are available:
+# debug_test_project_paths
+
 # Your tests begin here.  Here's an example.
 # Load the project's bash_lib.sh
 # . "$project_bin_dir/bash_lib.sh"
-
 
