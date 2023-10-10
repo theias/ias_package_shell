d4b60dc52e272b871bd65e964edb5686944bc22a
Martin VanWinkle III <mvanwinkle@ias.edu>
2023-10-10 13:11:56 -0400

    progress

--- src/templates/project_dir/src/bin/[% project.package_name %].sh
+++ src/templates/project_dir/src/bin/[% project.package_name %].sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+all_arguments=( "$@")
+
 date
-echo "Hello."
+printf '%s\n' "Hello."
+all_arguments_string="${all_arguments[@]}"
+
+printf "%s: %s\n" 'All arguments' "$all_arguments_string"
+
--- src/templates/project_dir/tests/bin/test_[% project.package_name %].sh
+++ src/templates/project_dir/tests/bin/test_[% project.package_name %].sh
@@ -2,6 +2,8 @@
 
 set -e
 
+all_arguments=( "$@" )
+
 # Bootstrap testing infrastructure
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 my_lib_dir=${my_lib_dir:-"$this_dir/../lib"}
@@ -12,7 +14,25 @@ my_lib_dir=${my_lib_dir:-"$this_dir/../lib"}
 # This dumps some of the variables that are available:
 # debug_test_project_paths
 
-# Your tests begin here.  Here's an example.
+# This is where you specify what is to be called,
+# along with its arguments:
 
-"$project_bin_dir/[% project.package_name %].sh"
+test_command=( \
+	"$project_bin_dir/[% project.package_name %].sh" \
+	# Put your arguments here:
+	arg1,
+	arg2
+)
+
+# We determine if we were called with any arguments
+if ((${#all_arguments[@]}))
+then
+	# If we were, then we add them to the end of our
+	# test_command array
+	test_command+=( "${all_arguments[@]}")
+fi
+
+# Then we run the test command.
+"${test_command[@]}"
+
 
